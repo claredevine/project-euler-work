3dd9366ad0232a0ddb44837bed1b1786ebedf285
clare.devine <clare.devine@sky.uk>
2017-06-07 13:00:48 +0100

    rework

---
+++ problem1/multiplesOf3And5.js
@@ -0,0 +1,13 @@
+function multiplesOfThreeAndFive() {
+  for(var i = 1; i < 1000; i++) {
+    if(i % 3 == 0) {
+      console.log(i);
+      }
+    }
+
+    for(var j = 1; j < 1000; j++) {
+      if(j % 5 == 0) {
+        console.log(j);
+      }
+    }
+}
